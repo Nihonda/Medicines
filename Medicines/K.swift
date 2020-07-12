0935f744acdd556bd711e921f5eb5ee66bc9d7f6
Nurlan <carjapancar@gmail.com>
2020-07-12 17:19:27 +0600

    Add files IO.swift, K.swift to work with files

---
+++ Medicines/K.swift
@@ -0,0 +1,15 @@
+//
+//  K.swift
+//  Medicines
+//
+//  Created by Apple on 12/7/20.
+//  Copyright © 2020 Nihonda. All rights reserved.
+//
+
+import Foundation
+
+class K {
+    class File {
+        static let QUERIES_JSON     = "queries.json"
+    }
+}
